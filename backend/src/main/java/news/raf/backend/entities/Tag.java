783359fe2e69e3d8953171240f6fb060697ba6ba
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-31 17:25:52 +0200

    Add getters and setters to Tag

--- backend/src/main/java/news/raf/backend/entities/Tag.java
+++ backend/src/main/java/news/raf/backend/entities/Tag.java
@@ -22,4 +22,28 @@ public class Tag {
 
     public Tag() {
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
