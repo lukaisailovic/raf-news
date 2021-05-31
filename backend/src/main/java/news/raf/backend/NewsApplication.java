76b3224391e4e7795f30a0381e08e64da23f6318
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-31 16:32:59 +0200

    Initial commit

---
+++ backend/src/main/java/news/raf/backend/NewsApplication.java
@@ -0,0 +1,9 @@
+package news.raf.backend;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class NewsApplication extends Application {
+
+}
