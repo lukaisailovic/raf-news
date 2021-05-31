a76bd875f62036851d2a031baa59e9df3edf72d6
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-31 16:50:53 +0200

    Change description in category to not be nullable

--- backend/src/main/java/news/raf/backend/entities/Category.java
+++ backend/src/main/java/news/raf/backend/entities/Category.java
@@ -17,6 +17,6 @@ public class Category {
     @Column(name = "name", unique = true, nullable = false)
     private String name;
 
-    @Column(name = "description", nullable = true)
+    @Column(name = "description", nullable = false)
     private String description;
 }
