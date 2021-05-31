1d946bfe763c6e9db7b6576e7f72319988597c82
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-31 16:49:59 +0200

    Add Category entity

---
+++ backend/src/main/java/news/raf/backend/entities/Category.java
@@ -0,0 +1,22 @@
+package news.raf.backend.entities;
+
+
+import org.eclipse.persistence.annotations.UuidGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@UuidGenerator(name="CATEGORY_ID_GEN")
+public class Category {
+
+    @Id
+    @GeneratedValue(generator="USER_ID_GEN")
+    private String id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+}
