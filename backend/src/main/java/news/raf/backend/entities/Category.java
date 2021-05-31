d850921cca3d8714023528aea9788077f8c55586
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-31 17:05:59 +0200

    Create getters, setters and constructor in Category entity

--- backend/src/main/java/news/raf/backend/entities/Category.java
+++ backend/src/main/java/news/raf/backend/entities/Category.java
@@ -19,4 +19,31 @@ public class Category {
 
     @Column(name = "description", nullable = false)
     private String description;
+
+    public Category() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
