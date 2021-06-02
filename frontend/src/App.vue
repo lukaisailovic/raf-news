a59707326e9bc2f590b331b8e5c66f5f4216b303
Luka Isailovic <lukaisailovic@gmail.com>
2021-06-02 21:15:53 +0200

    Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+    <div id="app">
+        <Navbar/>
+        <b-container class="mt-4">
+            <b-row>
+                <b-col>
+                    <router-view/>
+                </b-col>
+            </b-row>
+        </b-container>
 
+    </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
 <style>
 
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-container>
+                <b-navbar-brand href="#">{{ appName }}</b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item to="/">Home</b-nav-item>
+                        <b-nav-item to="/">Popular</b-nav-item>
+                        <b-nav-item to="/">Categories</b-nav-item>
+                    </b-navbar-nav>
+
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-item-dropdown right>
+                            <template #button-content>
+                                <em>User</em>
+                            </template>
+                            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-container>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    computed: {
+        appName() {
+            return process.env.VUE_APP_NAME;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
