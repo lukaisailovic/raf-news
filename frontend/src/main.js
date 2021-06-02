fbc7a1744433d63e67fa599c94379b1756680752
Luka Isailovic <lukaisailovic@gmail.com>
2021-06-02 20:58:01 +0200

    Add frontend dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
